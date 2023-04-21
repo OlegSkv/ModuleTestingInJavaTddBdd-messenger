41bf6afd5294575f55173a178f1a8639b11b2a59
Oleg_Skvortcov <oleg_skvortcov@epam.com>
2023-04-21 11:58:15 +0300

    RED add test for 2nd requirement: If at least one placeholder value is not provided at runtime – template generator should throw an exception.

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,8 +1,12 @@
 package com.epam.ld.module2.testing.template;
 
 import com.epam.ld.module2.testing.Client;
+import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
+import org.junit.rules.ExpectedException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,20 +14,43 @@ import java.util.Map;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@EnableRuleMigrationSupport
 public class TemplateEngineTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private Template template;
+    private TemplateEngine templateEngine;
+
+    @BeforeEach
+    public void setup() {
+        template = new TemplateImpl("Hi, #{first}. Have a good #{second}");
+        templateEngine = new TemplateEngine();
+    }
 
     @Test
     public void templatePlaceholdersShouldBeReplacedWithRuntimeValues() {
-        Template template = new TemplateImpl("Hi, #{first}. Have a good #{second}");
         Client client = mock(Client.class);
         Map<String, String> runtimePlaceholders = new HashMap<>();
         runtimePlaceholders.put("#{first}", "Java");
         runtimePlaceholders.put("#{second}", "day");
         when(client.getPlaceholders()).thenReturn(runtimePlaceholders);
 
-        TemplateEngine templateEngine = new TemplateEngine();
         String message = templateEngine.generateMessage(template, client);
 
         Assertions.assertEquals("Hi, Java. Have a good day", message);
+    }
+
+    //If at least one placeholder value is not provided at runtime – template generator should throw an exception.
+    @Test
+    public void templateGeneratorShouldThrowAnExceptionWhenAtLeastOnePlaceholderAbsent() {
+        thrown.expect(RuntimeException.class);
+        Client client = mock(Client.class);
+        Map<String, String> runtimePlaceholders = new HashMap<>();
+        runtimePlaceholders.put("#{first}", "Java");
+        when(client.getPlaceholders()).thenReturn(runtimePlaceholders);
+
+        templateEngine.generateMessage(template, client);
     }
 }
