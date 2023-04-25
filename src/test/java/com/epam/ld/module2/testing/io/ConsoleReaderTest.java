b5957a3acf511890bdeff2b3f737a2d09d96ee38
Oleg_Skvortcov <oleg_skvortcov@epam.com>
2023-04-25 19:00:00 +0300

    RED add test to ConsoleReader

--- src/test/java/com/epam/ld/module2/testing/io/ConsoleReaderTest.java
+++ src/test/java/com/epam/ld/module2/testing/io/ConsoleReaderTest.java
@@ -24,4 +24,17 @@ class ConsoleReaderTest {
         assertTrue(placeholdersByValues.containsKey("#{firstPlaceholder}"));
         assertEquals(placeholdersByValues.get("#{firstPlaceholder}"), "firstValue");
     }
+
+    @Test
+    void getPlaceholdersByValuesShouldAllowSpacesBetweenWordsInPlaceholderValue() {
+        PrintStream out = System.out;
+        ByteArrayInputStream in =
+                new ByteArrayInputStream("firstPlaceholder\nfirst spaces in value \nn".getBytes(StandardCharsets.ISO_8859_1));
+        ConsoleReader consoleReader = new ConsoleReader(in, out);
+
+        Map<String, String> placeholdersByValues = consoleReader.getPlaceholdersToValues();
+
+        assertTrue(placeholdersByValues.containsKey("#{firstPlaceholder}"));
+        assertEquals(placeholdersByValues.get("#{firstPlaceholder}"), "first spaces in value ");
+    }
 }
